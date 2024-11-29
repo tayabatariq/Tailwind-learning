b6db53cd7672387f0619b33bfc88ce5ee13dbffb
tayabatariq <168851346+tayabatariq@users.noreply.github.com>
2024-11-29 11:53:53 +0500

    header part create

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-gray': '#171717', // Custom color name
+        'dark-charcoal': '#1B1B1B',
+        'custom': [
+          'Inter',
+          'ui-sans-serif',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+        'bright-yellow': '#FFFF00', 
+          'custom-gray': '#757575', // Custom color name
+      },
+    },
   },
   plugins: [],
 }
